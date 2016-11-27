fa560defdee9c670dc87a97c3c8662fedb45ce12
Agustin Chiappe Berrini <jnieve@gmail.com>
2016-11-27 09:43:31 -0500

    Try to fix deploy

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-node ./lib/main.js start --password ${ADMIN_PASS:-"default"} --mongo ${MONGO_URL:-"localhost/sbm"}
+node ./lib/main.js start --port ${PORT:-"3000"} --password ${ADMIN_PASS:-"default"} --mongo ${MONGO_URL:-"localhost/sbm"}
