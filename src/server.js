9a02db309ffa4e2cd6e18076fed2ff595a8c7267
Agustin Chiappe Berrini <jnieve@gmail.com>
2016-12-26 08:14:17 -0500

    Added cors support

--- src/server.js
+++ src/server.js
@@ -25,6 +25,12 @@ app.use(bodyParser.urlencoded())
 app.use(passport.initialize())
 passport.use(new LocalStrategy(User.authenticate()))
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
 router.post('/auth', user.authenticate, user.token)
 
 router.get('/user/:id', user.current)
